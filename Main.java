7b6bcf7ba4f1188572778b25c0fd45ecb0c82c60
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2024-01-16 16:48:40 -0300

    feat: remove, increase capacity.

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(10);
+        Vetor vetor = new Vetor(4);
 
-        vetor.adiciona("elemento 1");
-        vetor.adiciona("elemento 2");
-        vetor.adiciona("elemento 3");
+        vetor.adiciona("A");
+        vetor.adiciona("B");
+        vetor.adiciona("C");
+        vetor.adiciona("D");
 
         System.out.println(vetor.getTamanho());
-        vetor.adiciona1(0, "novo elemento");
-
+//        vetor.adiciona1(1, "novo elemento");
+        vetor.removeDeFatoUmElemento("C");
 //        System.out.println(vetor.busca("Elemento 3"));
+//        vetor.removeUmElemento(2);
         System.out.println(vetor);
+        System.out.println(vetor.getTamanho());
     }
 }
--- Vetor.java
+++ Vetor.java
@@ -18,7 +18,7 @@ public class Vetor {
 //        }
 //    }
     public boolean adiciona(String elemento) {
-
+        aumentaCapacidade();
         if (this.tamanho < this.elementos.length) {
             this.elementos[this.tamanho] = elemento;
             this.tamanho++;
@@ -28,6 +28,7 @@ public class Vetor {
     }
 
     public boolean adiciona1(int posicao, String elemento) {
+        aumentaCapacidade();
         if (!(posicao < this.tamanho && posicao >= 0)) {
             return false;
         }
@@ -42,6 +43,39 @@ public class Vetor {
         return false;
     }
 
+    public void removeUmElemento(int posicao){
+        if(!(posicao >= 0 && posicao < this.tamanho)){
+            System.out.println("Vai rolar não pai");
+        }
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i+1];
+        }
+        this.tamanho--;
+    }
+
+    public void removeDeFatoUmElemento(String elementoRemover) {
+        String[] novosElementos = new String[this.tamanho];
+        int index = 0;
+        int posicaoBuscada = busca(elementoRemover);
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.elementos[i] != this.elementos[posicaoBuscada]) {
+                novosElementos[index] = this.elementos[i];
+                index++;
+            }
+        }
+        this.elementos = novosElementos;
+        tamanho--;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            String[] novosElementos = new String[this.tamanho*2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                novosElementos[i] = this.elementos[i];
+            }
+            this.elementos = novosElementos;
+        }
+    }
 //    public String busca(int posicao) {
 //        if (!(posicao < this.tamanho && posicao >= 0)) {
 //            return "dá não filho";
@@ -63,7 +97,21 @@ public class Vetor {
 
     @Override
     public String toString() {
-        return "elementos=" + Arrays.toString(elementos);
 
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i=0; i<this.tamanho-1; i++){
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+        if (this.tamanho>0){
+            s.append(this.elementos[this.tamanho-1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
     }
 }
